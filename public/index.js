d93d2d6e66fbdf6261f1c3431855a03cf5c76472
Peter Weigman <phweigman@gmail.com>
2021-09-11 18:36:05 -0400

    added previous db.js file

--- public/index.js
+++ public/index.js
@@ -112,10 +112,6 @@ function sendTransaction(isAdding) {
   populateTable();
   populateTotal();
 
-  //function to save record if post fails
-  function saveRecord(transaction) {
-    
-  };
   
   // also send to server
   fetch("/api/transaction", {
